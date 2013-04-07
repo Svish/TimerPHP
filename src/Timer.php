de76712c0c4552846579c48861da55d64eb54a88
Torleif Berger <torleif@outlook.com>
2013-04-07 19:49:03 +0200

    Fixed crash when formatting negative memory delta

--- src/Timer.php
+++ src/Timer.php
@@ -156,7 +156,7 @@ class Timer
 		if($bytes == 0)
 			return sprintf('%.2f '.$symbols[0], 0);
 
-		$exp = floor(log($bytes) / log(1024));
+		$exp = floor(log(abs($bytes)) / log(1024));
 		return sprintf('%.2f '.$symbols[$exp], $bytes/pow(1024, floor($exp)));
 	}
 }
